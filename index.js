6d237ecadd83882f25797f4905fda68e43effb66
NBBaoZheng <98695489+NBBaoZheng@users.noreply.github.com>
2022-11-05 14:13:51 +0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).on("click","#download",function(){
+     window.location.href = "https://github.com/NBBaoZheng/BDrive/archive/refs/tags/0.01b.zip"
+})
+$(document).on("click","#src",function(){
+     window.location.href = "https://github/NBBaoZheng/BDrive/"
+})
